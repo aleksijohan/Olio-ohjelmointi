fc222ee5e9f89a896e3412fd0df5a05569c1760f
aleksijohan <allu.jussila@hotmail.com>
2025-10-29 18:09:29 +0200

    h1
    muutoksia

--- h1.cpp
+++ h1.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
-#include <iomanip>      
+#include <iomanip>
 #include <stdexcept>
-
+#include <limits>
+#include <windows.h>
 
 using namespace std;
-
 
 void calcSum(int a, int b) {
     cout << "Summa = " << a + b << endl;
 }
 
-
 void calcDiv(int a, int b) {
     if (b == 0) {
         cout << "Virhe - nollalla jako ei ole sallittua." << endl;
+    } else if (b < 0) {
+        cout << "Virhe - negatiivisella luvulla jako ei ole sallittua." << endl;
     } else {
         float result = static_cast<float>(a) / b;
         cout << "Osamäärä = " << fixed << setprecision(2) << result << endl;
     }
 }
 
-
 int retSum(int a, int b) {
     return a + b;
 }
 
-
 float retDiv(int a, int b) {
     if (b == 0) {
+        throw runtime_error("jakaja ei saa olla nolla.");
+    }
+    if (b < 0) {
         throw runtime_error("jakaja ei saa olla negatiivinen.");
     }
     return static_cast<float>(a) / b;
 }
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     int a, b;
 
-    
     cout << "Anna kokonaisluku a: ";
     cin >> a;
 
@@ -47,21 +51,22 @@ int main() {
     calcSum(a, b);
     calcDiv(a, b);
 
-   
+
     int summa = retSum(a, b);
     cout << "Summa = " << summa << endl;
 
+    
     try {
         float result = retDiv(a, b);
-        cout << "" << a << " / " << b << " = " << fixed << setprecision(2) << result << endl;
-    } catch (runtime_error& e) {
+        cout << a << " / " << b << " = " << fixed << setprecision(2) << result << endl;
+    } catch (const runtime_error& e) {
         cout << "Error: " << e.what() << endl;
     }
 
-    std::cout << "Paina Enter sulkeaksesi ohjelman...";
-    std::cin.ignore(); // Tyhjentää puskurin
-    std::cin.get();    // Odottaa Enteriä
-
+    
+    cout << "Paina Enter sulkeaksesi ohjelman...";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
 
     return 0;
 }
