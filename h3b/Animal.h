cee1237ce1b00742e06af8013c998e34888c81e0
aleksijohan <allu.jussila@hotmail.com>
2025-11-22 14:25:57 +0200

    Update Animal.h

--- h3b/Animal.h
+++ h3b/Animal.h
@@ -4,7 +4,7 @@
 class Animal {
 public:
     Animal();
-    virtual ~Animal();                // Tärkeä! Mahdollistaa oikean destruktorin kutsun periytyvissä luokissa
-
-    virtual void callOut() const;     // Virtuaalinen metodi → voidaan ylikirjoittaa
+    virtual ~Animal();               
+    virtual void callOut() const;    
 };
+
