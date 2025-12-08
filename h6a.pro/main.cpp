2415ce55b2b3f33711b6ed2b5919f7e2b9a15435
aleksijohan <allu.jussila@hotmail.com>
2025-12-08 14:13:07 +0200

    qt

---
+++ h6a.pro/main.cpp
@@ -0,0 +1,13 @@
+
+#include <QCoreApplication>
+#include "myclass.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    MyClass obj;
+    obj.raiseMySignal();
+
+    return a.exec();
+}
